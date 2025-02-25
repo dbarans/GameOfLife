f2d688e272c577b5d58c46b179e1e317a67bc1f7
Dominik Baranski <baranskidominik109@gmail.com>
2025-02-25 20:36:48 +0100

    Removed Adaptive Provider and implemented dynamic FPS display

---
+++ Assets/FPSCounter.cs
@@ -0,0 +1,28 @@
+using TMPro;
+using UnityEngine;
+
+public class FPSCounter : MonoBehaviour
+{
+    public TextMeshProUGUI fpsText;
+
+    private float deltaTime = 0.0f;
+    private float fps = 0.0f;
+    private void Start()
+    {
+        float refreshRate = (float)Screen.currentResolution.refreshRateRatio.value;
+        Application.targetFrameRate = !float.IsNaN(refreshRate) ? (int)refreshRate : 144; //for dev purposes, change to 60
+    }
+    void Update()
+    {
+
+        deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+        fps = 1.0f / deltaTime;
+
+        if (fpsText != null)
+        {
+            fpsText.text = "FPS: " + fps.ToString("F2");
+        }
+    }
+
+}
+
