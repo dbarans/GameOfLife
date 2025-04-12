441dbaab43df460da4e565692676e2371c749cd7
Dominik Barański <baranskidominik109@gmail.com>
2025-04-12 21:44:26 +0200

    Slide out animation for button panel

---
+++ Assets/Scripts/ButtonPanelSlider.cs
@@ -0,0 +1,39 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ButtonPanelSlider : MonoBehaviour
+{
+    private RectTransform rectTransform;
+    private Vector2 extendedPosition;
+    [SerializeField] private Vector2 hiddenPosition;
+    [SerializeField] private float animationDuration;
+    [SerializeField] private GameObject slideInButton;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        extendedPosition = rectTransform.anchoredPosition;
+        rectTransform.anchoredPosition = hiddenPosition;
+    }
+
+    public void SlideOut()
+    {
+        rectTransform.DOAnchorPos(hiddenPosition, animationDuration).SetEase(Ease.InBack)
+            .OnComplete(() =>
+            {
+                slideInButton.SetActive(true);
+            });
+        
+    }
+    public void SlideIn()
+    {
+        slideInButton.SetActive(false);
+        rectTransform.DOAnchorPos(extendedPosition, animationDuration).SetEase(Ease.OutBack);
+    }
+
+
+}
